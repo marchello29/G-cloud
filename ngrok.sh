4e28cf9259059408a9cac0cda5ac671697feb730
marchello29 <111630727+marchello29@users.noreply.github.com>
2022-10-02 15:37:57 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP 
+read -p "Ctrl + V Your Authtoken: " tokenngrok
+./ngrok authtoken $tokenngrok
