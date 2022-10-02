d910ad141932bf88178afc593d453c69a777ee8b
marchello29 <111630727+marchello29@users.noreply.github.com>
2022-10-02 20:42:42 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,4 +1,4 @@
-rm ng.sh
+rm -rf ng.sh
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 echo "================================"
