e6bbd12769f77890ac0486feef58743cc9b5fcc7
marchello29 <111630727+marchello29@users.noreply.github.com>
2022-10-02 16:52:05 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -5,7 +5,7 @@ echo "================================"
 echo "Use fresh token from Ngrok to anticipate the occurrence of errors when starting a new ngrok tunnel"
 echo "Recommended you Reset Authtoken before use this RDP"
 echo "Blur your AUTHTOKEN from anyone if you take this video"
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip &>/dev/null &
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 read -p "Do you understand from this disclamer? [Y/n]: " disclamerngrok
 if [[ $disclamerngrok == "y" || $disclamerngrok == "yes" || $disclamerngrok == "1" ]]
